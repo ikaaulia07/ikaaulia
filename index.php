fb61d8e244c811cdcc34f81192523fc11a7a194e
ikaaulia07 <ikaaulia519@gmail.com>
2026-04-15 20:38:49 +0800

    commit pertamaku

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$nama="abdul";
+$umur=20;
+$tinggi=170.5;
+$menikah=true;
+$hoby=["membaca","berenang","bermain gitar"];
+
+echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi cm, status saya $menikah";
+?>
